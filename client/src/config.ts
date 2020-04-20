63ee884f5c7bc48d05f34fd0ecb665b64707c9d3
Nikolas Kaloumenos <nakaloumenos@gmail.com>
2020-04-20 20:37:59 +0100

    Ready for submit (#6)
    * Initial structure (#1)
    
    * initialize backend structure
    
    * copy client from todo project
    
    * move files
    
    * add login functionality (#2)
    
    * create pets with metadata and get them (#3)
    
    * My pets (#4)
    
    * add user pet and delete func
    
    * add upload pic functionality
    
    * Final functionality (#5)
    
    * add final buttons
    
    * final fixes
    
    * update readme file

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '	rjejxnn39a'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'nakaloumenos.auth0.com', // Auth0 domain
+  clientId: 'EfI2dI9xxADH2DAhXrHNCQozAy1vCP5B', // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
